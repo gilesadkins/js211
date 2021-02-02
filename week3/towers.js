dfb8b60eb1e3038f3f03e9680e6d6c61ecdfdc8b
gilesadkins <gilesadkins@gmail.com>
2021-02-02 15:26:46 -0600

    finished towers of hanoi

--- week3/towers.js
+++ week3/towers.js
@@ -31,6 +31,7 @@ let stacks = {
 };
 
 // Start here. What is this function doing?
+// This function console logs the arrays inside of the stacks object. Showing the player the board.
 const printStacks = () => {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
@@ -38,9 +39,9 @@ const printStacks = () => {
 }
 
 // Next, what do you think this function should do?
+// This function will note be checking if the move is legal. All it will do is move the piece from the starting stack to the ending stack.
 /**
- * This function will note be checking if the move is legal. All it will do is move the piece from the starting stack to the ending stack.
- * 
+ * The pop() and push() array methods pull of the end of an array which will always be the smallest piece once we have our isLegal() function working.
  * @param {*} startStack this parameter will be the stack the piece is moving from. This will be used with the pop() method, which
  * we will store the return value from in a variable called piece.
  * @param {*} endStack this parameter will take the piece variable, and use the push() method on the corresponding array.
@@ -52,8 +53,14 @@ const movePiece = (startStack, endStack) => {
 
 // Before you move, should you check if the move it actually allowed? Should 3 be able to be stacked on 2
 /**
- * returns true if it is a legal move, and false if not legal.
- * end stack
+ * This function returns true if it is a legal move, and false if not legal.
+ * The first thing to check is if the startStack(the stack where the piece is moving from) has a piece to move. 
+ * Because you cannot move a piece that doesn't exist.
+ * Then we can test if the place we are moving to has no pieces. We do these two tests first because slice will be undefined if the arrays have no elements.
+ * IF(if statment) that is true, move on to next test. IF not true, return true.
+ * The next test is to check IF the peice being moved is smaller than the piece it is being stacked upon.
+ * Originally I tried to find a solution with .length - 1 but found it was easier to use slice(-1) to pull the end value of the array and compare them.
+ * If true return TRUE. Else return false.
  */
 const isLegal = (startStack, endStack) => {
   if(stacks[startStack].length > 0){
@@ -66,41 +73,43 @@ const isLegal = (startStack, endStack) => {
     return false
     }
   }
-}
-
+};
 // What is a win in Towers of Hanoi? When should this function run?
+// A win is when all of the pieces are LEGALLY stacked in a stack other than the starting stack.
 /**
- * this function returns true if board is in winning state and false if not.
+ * This function returns true if board is in winning state and false if not.
+ * All that needs to be done here is check if stack B or C is full of pieces.
+ * We dont have to worry about how they got there since we have a seperate isLegal function.
+ * An if statement that checks if B OR C is full (array length of 4) will work.
+ * If A OR B have a length of 4. Return true. Else return false.
  */
 const checkForWin = () => {
-  if(stack[b].length == 4|| stack[c].length == 4) {
+  if(stacks['b'].length == 4|| stacks['c'].length == 4) {
     return true
   } else {
     return false
   }
-}
+};
 /**
- * this function is the brains and drives 1 turn of the game
+ * This function is meant to put all of the working peices of the game together in the correct order to execute one turn of the game.
  * @param {} startStack 
  * @param {*} endStack 
+ * First it should check IF(if statment) the move is legal.
+ * If ifLegal() is false, print not legal message.
+ * If ifLegal() is true, execute movePiece().
+ * After completing the if statment. The check for win function will then check IF(if statment) the board object is in a winning state.
+ * If checkForWin() is true. Print winning message. If false do nothing.
  */
-// When is this function called? What should it do with its argument?
+
 const towersOfHanoi = (startStack, endStack) => {
-  // check if move is legal
   if(isLegal(startStack, endStack)){
     movePiece(startStack, endStack)
   } else {
     return console.log('Move is not legal.')
   }
-
   if (checkForWin()) {
     return console.log('You have won!')
-  }
-  // if move is not then print a not legal message.
-  //if move it legal make move
-  
-  //after make move check if user won
-  //if board is in win state print winning message
+  } 
 }
 
 const getPrompt = () => {
@@ -113,7 +122,7 @@ const getPrompt = () => {
   });
 }
 
-// Tests
+
 
 if (typeof describe === 'function') {
 
@@ -150,6 +159,40 @@ if (typeof describe === 'function') {
       assert.equal(checkForWin(), false);
     });
   });
+
+  describe('#checkTurns', () => {
+    it('should detect if user has gone over 10 turns', () => {
+      let actual = checkTurns(10)
+      let expected = "You are so close!"
+      assert.equal(actual, expected);
+    });
+  });
+
+
+  describe('#translateOnomatopoeia', () => {
+    it('shoud take in ayy, bee, and cee', () => {
+      let actual = towersOfHanoi('ayy', 'bee')
+      let expected = towersOfHanoi('a', 'b')
+      assert.equal(actual, expected);
+    });
+  });
+
+  describe('#translateNumbers', () => {
+    it('inputs a, b and c should be equal to 1, 2 and 3', () => {
+      let actual = towersOfHanoi('1', '2')
+      let expected = towersOfHanoi('a', 'b')
+      assert.equal(actual, expected);
+    });
+  });
+
+  describe('#uppercaseLetters', () => {
+    it('uppercase inputs should work', () => {
+      let actual = towersOfHanoi('A', 'B')
+      let expected = towersOfHanoi('a', 'b')
+      assert.equal(actual, expected);
+    });
+  });
+
 
 } else {
 
