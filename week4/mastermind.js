1f97b3da2d08bc291a6cf3a94b208062d7b7073c
gilesadkins <gilesadkins@gmail.com>
2021-02-08 20:33:57 -0600

    mastermind

--- week4/mastermind.js
+++ week4/mastermind.js
@@ -7,6 +7,15 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+/**
+ *  4 Letter code is created "a,b,c,d" with generate solution.
+ *  Player guesses what the code is.
+ *  A hint will be generated based off of the guess and the solution.
+ *  The first hint will tell the player how many letters are exactly correct.
+ *  Exactly correct means correct letter and position.
+ *  The second hint will only tell the player how many letters are correct not if positions are right.
+ */
+
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
@@ -28,13 +37,54 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+// [ a , b , c , d] guess
+// if guess[0] is equal to guess[0] add 1 to hint1
+// for loop that checks guess[i] and solution[i]
+let guessArr = guess.split("");
+let solutionArr = solution.split("");
+
+let hint1 = 0;
+let hint2 = 0;
+
+for(let i = 0; i < 4; i++){
+  if(guessArr[i] == solutionArr[i]){
+    hint1 = hint1 + 1;
+    guessArr[i] = 1;
+    solutionArr[i] = 0;
+  }
+}
+
+for(let i = 0; i < 4; i++){
+  for(let i2 = 0; i2 < 4; i2++){
+    if(guessArr[i] == solutionArr[i2]){
+      hint2 = hint2 + 1;
+      guessArr[i] = 1;
+      solutionArr[i2] = 0;
+    }
+    }
+  }
+
+
+return hint1 + "-" + hint2;
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  if(guess == solution) {
+    board = [];
+    console.log(`You guessed it!`)
+    return `You guessed it!`
+  } else {
+    console.log(generateHint(guess));
+    board.push(guess);
+  }
+  
+  if(board.length > 9){
+    console.log(`GAME OVER`)
+    board = [];
+    solution = "";
+    generateSolution();
+  }
 }
 
 
